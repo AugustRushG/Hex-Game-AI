5fc847a42d393be5c041414002b732f8ac0de6a2
HaoXu <hxxu4@student.unimelb.edu.au>
2022-04-29 18:55:18 +1000

    Regular update

--- Augustian/player.py
+++ Augustian/player.py
@@ -20,6 +20,7 @@ class Player:
     blueStartList = []
 
     START_BOUND = 2
+    turns_left = 0
 
     color = ""
     boardSize = 0
@@ -39,6 +40,7 @@ class Player:
         """
         # put your code here
         self.boardSize = n
+        self.turns_left = n * n
 
         # draw the board
         for x in range(n):
@@ -114,41 +116,31 @@ class Player:
 
         # put your code here
         decision = ()
-        if self.color == "red":
-            print("count is ", self.count)
-            if self.count == 0:
-                decision = ('PLACE', 2, 3)
-                self.count += 1
-                s = self.evaluation()
-            elif self.count == 1:
-                decision = ('PLACE', 1, 3)
-                self.count += 1
-                s = self.evaluation()
-            else:
-                decision = ('PLACE', 0, 3)
-                self.count += 1
-                s = self.evaluation()
 
-        elif self.color == "blue":
-            print("count is ", self.count)
-            if self.count == 0:
-                decision = ('PLACE', 0, 3)
-                self.count += 1
-                b = self.evaluation()
-            else:
-                decision = ('PLACE', 1, 1)
-                self.count += 1
-                b = self.evaluation()
+        # print("still have this many cells that can be placed", turns_left, len(self.redOccupiedList),len(self.blueOccupiedList))
+
+        if self.color == "red":
+            # print("count is ", self.count)
+            cell = self.minimax(1, "red")
+
+            decision = ('PLACE', cell[0], cell[1])
+            # print(decision)
+
+        else:
+            # print("count is ", self.count)
+            cell = self.minimax(1, "blue")
+
+            decision = ('PLACE', cell[0], cell[1])
 
         return decision
 
     def turn(self, player, action):
         """
-        Called at the end of each player's turn to inform this player of 
-        their chosen action. Update your internal representation of the 
-        game state based on this. The parameter action is the chosen 
-        action itself. 
-        
+        Called at the end of each player's turn to inform this player of
+        their chosen action. Update your internal representation of the
+        game state based on this. The parameter action is the chosen
+        action itself.
+
         Note: At the end of your player's turn, the action parameter is
         the same as what your player returned from the action method
         above. However, the referee has validated it at this point.
@@ -159,47 +151,50 @@ class Player:
 
         if self.color == "red" and player == "red":
             self.redOccupiedList.append([action[1], action[2]])
-            print("red List recording self")
-            print(self.redOccupiedList)
-        elif self.color == "blue" and player == "red":
-            self.redOccupiedList.append([action[1], action[2]])
 
             if [action[1], action[2]] in self.blueStartList[0]:
                 self.blueStartList[0].remove([action[1], action[2]])
-                print("blue list start and goal", self.blueStartList, self.blueGoalList)
+                # print("blue list start and goal", self.blueStartList, self.blueGoalList)
+            if [action[1], action[2]] in self.blueStartList[1]:
+                self.blueStartList[1].remove([action[1], action[2]])
+                # print("blue list start and goal", self.blueStartList, self.blueGoalList)
+            # print("red List recording self")
+            # print(self.redOccupiedList)
+        elif self.color == "blue" and player == "red":
+            self.redOccupiedList.append([action[1], action[2]])
+            # print(self.blueStartList[1])
+            # print(action)
+            if [action[1], action[2]] in self.blueStartList[0]:
+                self.blueStartList[0].remove([action[1], action[2]])
+                # print("blue list start and goal", self.blueStartList, self.blueGoalList)
+            if [action[1], action[2]] in self.blueStartList[1]:
+                self.blueStartList[1].remove([action[1], action[2]])
+                # print("blue list start and goal", self.blueStartList, self.blueGoalList)
 
-            print("blue List recording red")
-            print(self.redOccupiedList)
+            # print("blue List recording red")
+            # print(self.redOccupiedList)
         elif self.color == "red" and player == "blue":
             self.blueOccupiedList.append([action[1], action[2]])
 
             # if blue placed at my goal or start cell, remove this cell from the list
             if [action[1], action[2]] in self.redStartList[0]:
                 self.redStartList[0].remove([action[1], action[2]])
-                print("red list start and goal", self.redStartList, self.redGoalList)
+                # print("red list start and goal", self.redStartList, self.redGoalList)
+            if [action[1], action[2]] in self.redStartList[1]:
+                self.redStartList[1].remove([action[1], action[2]])
 
-            print("red List recording blue")
-            print(self.blueOccupiedList)
+            # print("red List recording blue")
+            # print(self.blueOccupiedList)
         else:
             self.blueOccupiedList.append([action[1], action[2]])
-            print("blue List recording blue")
-            print(self.blueOccupiedList)
-
-    def decision(self, redOccList, blueOccList):
-        """
-        reads in where has been occupied, decide where to place based on the current board.
-        Args:
-            redOccList:
-            blueOccList:
-
-        Returns:
-
-        """
-        if self.color == "red":
-            result = ()
-        else:
-            result = ()
-        return result
+            if [action[1], action[2]] in self.blueStartList[0]:
+                self.blueStartList[0].remove([action[1], action[2]])
+                # print("blue list start and goal", self.blueStartList, self.blueGoalList)
+            if [action[1], action[2]] in self.blueStartList[1]:
+                self.blueStartList[1].remove([action[1], action[2]])
+                # print("blue list start and goal", self.blueStartList, self.blueGoalList)
+            # print("blue List recording blue")
+            # print(self.blueOccupiedList)
 
     def evaluation(self):
         """
@@ -216,12 +211,13 @@ class Player:
         Returns:
 
         """
-        print("red already occupied", self.redOccupiedList)
+
+        # print("red already occupied", self.redOccupiedList)
         red_score = self.get_shortest_path("red")
-        print("blue already occupied", self.blueOccupiedList)
+        # print("blue already occupied", self.blueOccupiedList)
         blue_score = self.get_shortest_path("blue")
 
-        print("evulating state", blue_score - red_score)
+        # print("evulating state", blue_score - red_score)
         # shortest path, start will be each two sides' coordinates, goal will be the same
         # by using A* search, work out the shortest path's, get this path's steps, minus already occupied cell number.
         # then get the shortest path number.
@@ -229,15 +225,78 @@ class Player:
         return blue_score - red_score
 
     def minimax(self, depth, maximizingPlayer):
+        print("minimax start")
         # if current board is has finished(win or lose or draw) or depth is 0 return
-        if depth == 0:
-            return
+        moves = self.get_all_possible_moves()
+        got_removed = False
+        got_removed1 = False
 
-    def get_path_score(self, path):
-        count = 0
-        for i in path:
-            count += 1
-        return count
+        if depth == 0:
+            return 0
+
+        if maximizingPlayer == "red":
+            max_evaluation = float('-inf')
+
+            for move in moves:
+                self.redOccupiedList.append(move)
+
+                if move in self.blueStartList[0]:
+                    self.blueStartList[0].remove(move)
+                    # print(self.blueStartList, self.blueGoalList)
+                    got_removed = True
+                if move in self.blueStartList[1]:
+                    self.blueStartList[1].remove(move)
+                    got_removed1 = True
+
+                evaluation = self.evaluation()
+
+                if evaluation > max_evaluation:
+                    max_evaluation = evaluation
+                    decision = move
+
+                self.redOccupiedList.remove(move)
+                if got_removed:
+                    self.blueStartList[0].append(move)
+                    got_removed = False
+                if got_removed1:
+                    self.blueStartList[1].append(move)
+                    got_removed1 = False
+
+            print("max is ", max_evaluation)
+            print("move is", decision)
+            return decision
+
+        else:
+            min_evaluation = float('inf')
+
+            for move in moves:
+                self.blueOccupiedList.append(move)
+
+                if move in self.redStartList[0]:
+                    self.redStartList[0].remove(move)
+                    got_removed = True
+
+                if move in self.redStartList[1]:
+                    self.redStartList[1].remove(move)
+                    got_removed1 = True
+
+                evaluation = self.evaluation()
+
+                if evaluation < min_evaluation:
+                    min_evaluation = evaluation
+                    decision = move
+
+                self.blueOccupiedList.remove(move)
+
+                if got_removed:
+                    self.redStartList[0].append(move)
+                    got_removed = False
+                if got_removed1:
+                    self.redStartList[1].append(move)
+                    got_removed1 = False
+            print("min is ", min_evaluation)
+            print("move is ", decision)
+            return decision
 
     def get_shortest_path(self, color):
         if color == "red":
@@ -260,13 +319,14 @@ class Player:
                             final = length
                             red_path = temp_path
 
-            print(red_path)
+            # print(red_path)
             # print("final length", final)
             return final
         else:
 
             blue_path = a_star_search(self.all_nodes, self.blueStartList[0][0], self.blueGoalList[0][0],
                                       self.redOccupiedList)
+
             final = len(blue_path)
             for i in range(self.START_BOUND):
                 for j in range(len(self.blueStartList[i])):
@@ -280,5 +340,12 @@ class Player:
                         if length < final:
                             final = length
                             blue_path = temp_path
-            print(blue_path)
+            # print("best path for blue",blue_path)
             return final
+
+    def get_all_possible_moves(self):
+        result = []
+        for move in self.all_nodes:
+            if (move not in self.redOccupiedList) and (move not in self.blueOccupiedList):
+                result.append(move)
+        return result
